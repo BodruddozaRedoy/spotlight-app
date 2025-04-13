7fed1f6c4ecefd0f8996ecc2adee5bfb7fb33a90
BodruddozaRedoy <bodruddozaredoy@gmail.com>
2025-04-13 16:47:57 +0600

    d

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,18 @@
 import { Text, View, StyleSheet, TouchableOpacity, Image } from "react-native";
 import { styles } from '../../styles/auth.styles';
 import { Link } from "expo-router";
+import { useAuth } from "@clerk/clerk-expo";
 
 export default function Index() {
+  const {signOut} = useAuth()
   return (
     <View>
       <Text>Feed screen</Text>
       <Link href={"/(auth)/login"} style={{backgroundColor: "blue", color: "white"}}>Login</Link>
+
+      <TouchableOpacity onPress={() => signOut()}>
+        <Text>Sign out</Text>
+      </TouchableOpacity>
     </View>
   );
 }
